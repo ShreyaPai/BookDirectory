1750c91c7dc4706fdf4555804434e7844370e027
Shreya Pai <ayerhs1997@gmail.com>
2021-10-10 20:46:15 +0530

    Add edit and delete routes

--- controller/book.js
+++ controller/book.js
@@ -1,4 +1,4 @@
-const Book = require('../models/book');
+const Book = require("../models/book");
 exports.addBook = async (req, res, next) => {
   try {
     console.log(req.body);
@@ -20,49 +20,104 @@ exports.addBook = async (req, res, next) => {
     });
   } catch (error) {
     if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      console.log(error)
-      next(error);
-    
+      error.statusCode = 500;
     }
-    
+    console.log(error);
+    next(error);
+  }
 };
 
 exports.getAllBooks = async (req, res, next) => {
   try {
     const books = await Book.find();
     res.status(200).json({
-      message: 'List of Books',
-      books: books
-    })
+      message: "List of Books",
+      books: books,
+    });
   } catch (error) {
     if (!error.statusCode) {
       error.statusCode = 500;
     }
-    console.log(error)
+    console.log(error);
     next(error);
   }
-}
+};
 
 exports.getBook = async (req, res, next) => {
   const bookId = req.params.bookId;
   try {
     const book = await Book.findById(bookId);
     if (!book) {
-      const err = new Error('Book Not Found');
-      const status = '404';
+      const err = new Error("Book Not Found");
+      const status = "404";
       throw err;
     }
     res.status(200).json({
-      message: 'Book Found',
-      book: book
-    })
+      message: "Book Found",
+      book: book,
+    });
   } catch (error) {
     if (!error.statusCode) {
       error.statusCode = 500;
     }
-    console.log(error)
+    console.log(error);
     next(error);
   }
-}
+};
+
+exports.editBook = async (req, res, next) => {
+  const bookId = req.params.bookId;
+  try {
+    const book = await Book.findById(bookId);
+    if (!book) {
+      const err = new Error("Book Not Found");
+      const status = "404";
+      throw err;
+    }
+    const newTitle = req.body.title;
+    const newAuthor = req.body.author;
+    const newSummary = req.body.summary;
+    const newDatePublished = req.body.datePublished;
+    book.title = newTitle;
+    book.author = newAuthor;
+    book.summary = newSummary;
+    book.datePublished = newDatePublished;
+    if (book._id.toString() === bookId.toString()) {
+      const updatedBook = await book.save();
+      res.status(200).json({
+        message: "Book Details Updated",
+        book: updatedBook,
+      });
+    }
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    console.log(error);
+    next(error);
+  }
+};
+
+exports.deleteBook = async (req, res, next) => {
+  const bookId = req.params.bookId;
+  try {
+    let book = await Book.findById(bookId);
+    if (!book) {
+      const err = new Error("Book Not Found");
+      const status = "404";
+      throw err;
+    }
+    if (book._id.toString() === bookId.toString()) {
+      await Book.findOneAndRemove(bookId);
+      res.status(200).json({
+        message: "Book Deleted",
+      });
+    }
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    console.log(error);
+    next(error);
+  }
+};
--- routes/book.js
+++ routes/book.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-const bookController = require('../controller/book')
+const bookController = require('../controller/book');
+
 router.post('/addBook', bookController.addBook);
 router.get('/getAllbooks', bookController.getAllBooks);
 router.get('/getBook/:bookId', bookController.getBook)
-
+router.put('/editBook/:bookId', bookController.editBook);
+router.delete('/deleteBook/:bookId', bookController.deleteBook)
 module.exports = router;
